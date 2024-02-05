b96d410e93dccd6c2c60d1163708f55b3a14fcd0
Taufique Iradukunda <taufiqueira@Taufiques-MacBook-Pro.local>
2024-02-04 21:19:13 -0500

    create new file that will contain codes for all inputs from the user

---
+++ lib/input_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'BMI CALCULATOR',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: Center(
+        child: Text('Body Text'),
+      ),
+    );
+  }
+}
