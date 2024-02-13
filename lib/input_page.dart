20a7d9bfc53facf69a04fa1834cc81f1f90df685
iradtaufique <iradukunda.ta@gmail.com>
2024-02-12 19:02:07 -0800

    customize slidertheme to match the design

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -98,17 +98,28 @@ class _InputPageState extends State<InputPage> {
                             )
                           ],
                         ),
-                        Slider(
-                            value: height.toDouble(),
-                            min: 120.0,
-                            max: 300.0,
-                            activeColor: Color(0xFFEB1555),
-                            inactiveColor: Color(0xFF8D8E98),
-                            onChanged: (double newHeight) {
-                              setState(() {
-                                height = newHeight.round();
-                              });
-                            })
+                        SliderTheme(
+                          data: SliderTheme.of(context).copyWith(
+                            inactiveTrackColor: Color(0xFF8D8E98),
+                            activeTrackColor: Colors.white,
+                            thumbColor: Color(0xFFEB1555),
+                            overlayColor: Color(0x29EB1555),
+                            thumbShape:
+                                RoundSliderThumbShape(enabledThumbRadius: 15.0),
+                            overlayShape:
+                                RoundSliderOverlayShape(overlayRadius: 30.0),
+                          ),
+                          child: Slider(
+                              value: height.toDouble(),
+                              min: 120.0,
+                              max: 300.0,
+                              inactiveColor: Color(0xFF8D8E98),
+                              onChanged: (double newHeight) {
+                                setState(() {
+                                  height = newHeight.round();
+                                });
+                              }),
+                        )
                       ],
                     ),
                   ),
